3ffe5235bd1b73c5caadb6a73be97ac77e7f7607
Shiramizu_Junya <jyunya.pg.7231@gmail.com>
2026-02-07 11:58:53 +0900

    URLパターンの修正と現在の日時を返すAPIエンドポイントを追加

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from . import views
 
-app_name = 'api'
+app_name = "api"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("current-datetime/", views.current_datetime, name="current_datetime"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from datetime import datetime
+
 
 def index(request):
     # return HttpResponse("<h1>Welcome to the API Index Page</h1>")
     return JsonResponse({"message": "Welcome to the API Index Page"})
+
+
+@api_view(["GET"])
+def current_datetime(request):
+    """現在の日時を返すAPIエンドポイント"""
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return Response({"current_datetime": current_time})
