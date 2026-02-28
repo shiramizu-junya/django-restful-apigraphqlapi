c972008c65244b6bf433bef11177b3851045c1b0
Shiramizu_Junya <jyunya.pg.7231@gmail.com>
2026-03-01 07:38:47 +0900

    Itemモデルを作成し、初期マイグレーションを追加

--- api_view_class/models.py
+++ api_view_class/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    price = models.IntegerField()
+    count = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
